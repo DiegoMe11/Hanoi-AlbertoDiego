33bb12ce1952b38d32c47d53ab6209506eb76c48
DiegoMe11 <115550176+DiegoMe11@users.noreply.github.com>
2023-03-13 17:01:55 +0100

    Pila implementada

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,36 +8,71 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        public int Size { get; set; } //Guarda el tamaño de la pila
+        public int Top { get; set; } //Guarda el elemento de arriba de la pila
+        public List<Disco> Elementos { get; set; } //List donde guarda los discos de la pila
 
-        /* TODO: Implementar métodos */
-        public Pila()
+
+        public Pila() //Inicializa la pila
         {
+            Top = 0;
+            Size = 0;
+            Elementos = new List<Disco>();
+        }
+        public Pila(int Size) //Añade las variables a la pila
+        {
+            this.Size = Size;
+            Elementos = new List<Disco>();
+
+            for (int i = this.Size; i > 0; i--)
+            {
+                Elementos.Add(new Disco(i));
+                if (i == 1)
+                {
+                    Top = Elementos[i].Valor;
+                }
+            }
+        }
+
+        public void push(Disco d) //Añade un disco a la pila y actualiza top y size
+        {
+            Elementos.Add(d);
+            this.Size++;
+            Top = Elementos[0].Valor;
 
         }
 
-        public void push(Disco d)
+        public Disco pop() //Elimina un disco a la pila y actualiza top y size
         {
+            Disco disco = Elementos.Last();
+            Elementos.Remove(disco);
 
+            if (Elementos.Count == 0) //Si no hay discos en una pila top = 0
+            {
+                Top = 0;
+            }
+            else
+            {
+                Top = Elementos[0].Valor;
+            }
+            this.Size--;
+            return disco;
         }
 
-        public Disco pop()
+        public bool isEmpty() //True si esta vacia, false si hay algo
         {
-            return null;
-        }                
-
-        public bool isEmpty()
-        {
-            return true;
+            bool devuelve;
+            if (Elementos.Count == 0)
+            {
+                devuelve = true;
+            }
+            else
+            {
+                devuelve = false;
+            }
+            return devuelve;
         }
 
     }
 }
+
